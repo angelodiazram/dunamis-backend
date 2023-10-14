998075a07f79e6c1baef4554f4a1a0c8af7c1344
Angelo Diaz <angelodiazram@gmail.com>
2023-10-13 21:18:40 -0300

    sin cors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import usuariosRouter from './src/routes/user.routes.js';
 import { db } from './config/db.config.js';
 
 import dotenv from 'dotenv';
-import { corsOptions } from './src/middlewares/cors.middleware.js';
+// import { corsOptions } from './src/middlewares/cors.middleware.js';
 
 dotenv.config();
 
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // middleware CORS:
-app.use(cors(corsOptions));
+app.use(cors());
 // middlewares de rutas:
 app.use('/api/v1', polerasRouter)
 app.use('/api/v1', usuariosRouter)
