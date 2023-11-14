0068c12afe479db2ef7920343c072dcefb6911a8
Angelo Diaz <angelodiazram@gmail.com>
2023-11-14 01:18:07 -0300

    propiedad 'description' inlcuida en Polera.model

--- src/models/Polera.model.js
+++ src/models/Polera.model.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema
 
 const poleraSchema = new Schema({
     urlImg: {type: String, require: true},
+    description: {type: String, require: true},
     color: {type: String, require: true},
     talla: {type: String, require: true},
     precio: {type: Number, require: true},
